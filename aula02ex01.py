a431f03fe6eb6f4023ba83f0843aedc4e632ed42
andrefrez02 <andreluizfrezarinidiasdasilva@gmail.com>
2022-03-08 23:29:21 -0300

    commit

---
+++ aula02ex01.py
@@ -0,0 +1,10 @@
+b, h, p, a = 0.0, 0.0, 0.0 , 0.0
+
+print('ÁREA e PERIMETRO do retângulo')
+b = float(input('Digite o valor da base do seu retângulo: '))
+h = float(input('Digite o valor da altura do seu retângulo: '))
+
+p = 2 * (b + h)
+a = b * h
+
+print(f'A Área do retângulo é de: {a}cm².\nE o Perimetro do retângulo é de: {p}cm.')
