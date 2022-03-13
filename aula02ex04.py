bc57944c33728908c728422ed3f53d1ebaebc57f
André <67207390+andrefrez02@users.noreply.github.com>
2022-03-13 19:11:28 -0300

    Add files via upload

--- aula02ex04.py
+++ aula02ex04.py
@@ -1,7 +1,9 @@
-a, b, c, x, resultado = 0, 0, 0, 0, 0
-A, B, D, sqrt, F, G = 0, 0, 0, 0, 0, 0
+import math
+
+a, b, c = 0, 0, 0
+aa, bb, dd, raizz, ff, gg = 0, 0, 0, 0, 0, 0
 x1, x2 = 0, 0
-H, J, K, M = 0, 0, 0, 0
+hh, kk = 0, 0
 delta = '\u0394'
 raiz = '\u221A'
 ml = '\u00B1'
@@ -17,28 +19,30 @@ b = int(input('Digite o valor de B: '))
 c = int(input('Digite o valor de C: '))
 
 print(f'\n{a}x²+{b}x+{c} = 0\n')
-print(f'{delta} = {b}²-4.{a}.{c}')
+print(f'{delta} = {b}²-4*{a}*{c}')
 
-B = (b**2)
-A = eval('-4*a*c')
-print(f'{delta} = {B} + {A}')
-D = eval('A+B')
-print(f'{delta} = {D}\n')
+bb = (b**2)
+aa = eval('-4*a*c')
+print(f'{delta} = {bb} + {aa}')
+dd = eval('aa+bb')
+print(f'{delta} = {dd}\n')
 
-print(f'x = -{b} {ml} {raiz}{D} / 2.{a}')
-sqrt = float(float(D) ** 0.5)
-G = 2 * a
-F = +(-b)
-print(f'x = {F} {ml} {sqrt} / {G}\n')
-H = eval('F+sqrt')
-x1 = H / G
-K = eval('F-sqrt')
-x2 = K / G
-print(f'x¹ = {F} + {sqrt} / {G}')
-print(f'x¹ = {H} / {G}')
-print(f'x¹ = {x1}\n')
-print(f'x² = {F} - {sqrt} / {G}')
-print(f'x² = {K} / {G}')
-print(f'x² = {x2}\n')
-print(f'S = \u007B{x1}, {x2}\u007D')
+print(f'x = -{b} {ml} {raiz}{dd} / 2*{a}')
+raizz = dd ** 0.5
+raizz = float(raizz.real)
+gg = 2 * a
+ff = +(-b)
+print(f'x = {ff} {ml} {raizz:0.3} / {gg}\n')
+hh = eval('ff+raizz')
+x1 = hh / gg
+kk = eval('ff-raizz')
+x2 = kk / gg
+
+print(f'x¹ = {ff} + {raizz:0.3} / {gg}')
+print(f'x¹ = {hh:0.3} / {gg}')
+print(f'x¹ = {x1:0.3}\n')
+print(f'x² = {ff} - {raizz:0.3} / {gg}')
+print(f'x² = {kk:0.3} / {gg}')
+print(f'x² = {x2:0.3}\n')
+print(f'S = \u007B{x1:0.3}, {x2:0.3}\u007D')
 input()
