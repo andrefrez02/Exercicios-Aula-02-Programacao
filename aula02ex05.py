9895533693fa62f22f448e2a0a40417bc14da637
andrefrez02 <andreluizfrezarinidiasdasilva@gmail.com>
2022-03-14 00:58:10 -0300

    commit

--- aula02ex05.py
+++ aula02ex05.py
@@ -7,3 +7,4 @@ real = float(input('Digite um valor em Reais para ser convertido em dólares: R$
 dolar = real / taxa
 
 print(f'O valor R${real} convertido para doláres fica: USD{dolar:0.3}')
+input()
--- aula02ex06.py
+++ aula02ex06.py
@@ -7,3 +7,4 @@ garcom = valor * 0.10
 valorF = valor + garcom
 
 print(f'Os 10% do garçom é igual a R${garcom:0.3} e somando esses 10%, você irá pagar R${valorF:0.3}')
+input()
